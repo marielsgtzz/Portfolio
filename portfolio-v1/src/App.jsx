804dc35cd2168ac6f5388215dfea00abf347b1d5
Marielsgtzz <marielsgtzz@gmail.com>
2023-06-04 19:24:43 -0700

    Add components to app

--- portfolio-v1/src/App.jsx
+++ portfolio-v1/src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Navbar from "./sections/navbar/Navbar";
+import Header from "./sections/header/Header";
+import About from "./sections/about/About";
+import Services from "./sections/services/Services";
+import Portfolio from "./sections/portfolio/Portfolio";
+import Testimonials from "./sections/testimonials/Testimonials";
+import Faqs from "./sections/faqs/Faqs";
+import Contact from "./sections/footer/Footer";
+import Footer from "./sections/footer/Footer";
+import FloatNavbar from "./sections/floating-navbar/FloatNavbar";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<>
+			<Navbar />
+			<Header />
+			<About />
+			<Services />
+			<Portfolio />
+			<Testimonials />
+			<Faqs />
+			<Contact />
+			<Footer />
+			<FloatNavbar />
+		</>
+	);
 };
 
 export default App;

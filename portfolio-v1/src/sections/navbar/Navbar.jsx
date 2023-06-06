dedf15a875f187c97cd2d2eaf0755c8e9f76dd21
Marielsgtzz <marielsgtzz@gmail.com>
2023-06-05 20:34:06 -0700

    Navbar

--- portfolio-v1/src/sections/navbar/Navbar.jsx
+++ portfolio-v1/src/sections/navbar/Navbar.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import Logo from "../../assets/logo.jpg";
+import data from "./data";
+import { IoIosColorPalette } from "react-icons/io";
 import "./navbar.css";
 
 const Navbar = () => {
 	return (
-		<section id="navbar">
-			<h2>Navbar</h2>
-		</section>
+		<nav>
+			<div className="container nav__container">
+				<a
+					href="index.html"
+					className="nav__logo"
+				>
+					<img
+						src={Logo}
+						alt="Logo"
+					/>
+				</a>
+				<ul className="nav__menu">
+					{data.map((item) => (
+						<li key={item.id}>
+							<a href={item.link}>{item.title}</a>
+						</li>
+					))}
+				</ul>
+				<button id="theme__icon">
+					<IoIosColorPalette />
+				</button>
+			</div>
+		</nav>
 	);
 };
 
